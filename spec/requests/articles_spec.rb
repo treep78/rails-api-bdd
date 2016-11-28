af867eca4997cda96024bf240a6c25f38e77adb1
treep78 <treep78@gmail.com>
2016-11-28 10:11:26 -0500

    comment tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Articles API' do
   def article_params
+    # parameters for creating an article
     {
       title: 'One Weird Trick',
       content: 'You won\'t believe what happens next...'
@@ -9,28 +10,42 @@ RSpec.describe 'Articles API' do
   end
 
   def articles
+    # returns a list of all articles
     Article.all
   end
 
   def article
+    # returns the first article
     Article.first
   end
 
   before(:all) do
+    # before each test, create an article
+    # using the article params (above)
     Article.create!(article_params)
   end
 
   after(:all) do
+    # delets the article after all tests have run
     Article.delete_all
   end
 
+  # feature test
+  # we're describing what happens when a get request is made to /articles
   describe 'GET /articles' do
+    # we expect the get request to return a list of all the articles
     it 'lists all articles' do
+      # get is a function that takes a string, and
+      # makes a get request to that adress
       get '/articles'
-
+      # expect to get a 20(0/x) response
       expect(response).to be_success
 
+      # sets the vaiable 'articles_resposne' to
+      # the body of the http response
       articles_response = JSON.parse(response.body)
+      # expect the list of articles that we get from the server to
+      # be the same length as the list of articles we created
       expect(articles_response.length).to eq(articles.count)
       expect(articles_response.first['title']).to eq(article['title'])
     end
